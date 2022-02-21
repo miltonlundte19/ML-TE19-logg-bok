fc939c5bace9dcf1e6efe2268fe62d6086bfdde3
miltonlundte19 <milton.lund@elev.ga.ntig.se>
2022-02-21 09:26:53 +0100

    början på guit

---
+++ src/view/loggBokView.java
@@ -0,0 +1,13 @@
+package view;
+
+import javax.swing.*;
+
+public class loggBokView {
+    private JPanel panel1;
+    private JButton button1;
+    private JTextField textField1;
+    private JTextPane textPane1;
+
+    private JMenuBar menuBar;
+    private JMenu menu;
+}
