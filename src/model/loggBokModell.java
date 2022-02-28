87ed2fd3b77f053cfe88ce8760c72c6e719c60f1
miltonlundte19 <milton.lund@elev.ga.ntig.se>
2022-02-28 09:40:03 +0100

    har börjat på att skriva om funktionerna i modellen för att få dom att fungera

--- src/model/loggBokModell.java
+++ src/model/loggBokModell.java
@@ -2,17 +2,67 @@ package model;
 
 import loggEntryClass.loggEntry;
 
+import javax.swing.*;
 import java.io.*;
+import java.nio.channels.FileChannel;
 import java.util.ArrayList;
 
 public class loggBokModell {
     private ArrayList<loggEntry> loggentrys;
     private File loggfile;
+    private Boolean saved = true;
 
     public loggBokModell() {
-        loggfile = new File("loggentryse");
-        loadS();
+        loggfile = new File("loggentryse", ".txt");
+        beginModell();
     }
+
+    private void beginModell() {
+        if (loggfile.exists()) {
+            try {
+                ObjectOutputStream ins = new ObjectOutputStream(new FileOutputStream(loggfile));
+                loggentrys = ( (ArrayList<loggEntry>) ins.readObject() );
+                ins.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            loggentrys = new ArrayList<loggEntry>();
+            saved = false;
+        }
+    }
+
+    private void save() {
+        try {
+            ObjectOutputStream outs = new ObjectOutputStream(new FileOutputStream(loggfile));
+            outs.writeObject(loggentrys);
+            outs.flush();
+            outs.close();
+            saved = true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private void saveAS() {
+        JFileChooser chooser = new JFileChooser();
+        chooser.setCurrentDirectory(new java.io.File("."));
+        chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        String curentFile = loggfile.getName();
+        if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+            // test som ser om filens namn fungerar som ett namn på en fill.
+        }
+        try {
+            ObjectOutputStream outs = new ObjectOutputStream(new FileOutputStream(loggfile));
+            outs.writeObject(loggentrys);
+            outs.flush();
+            outs.close();
+            saved = true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private void lode() {}
+
 
     private void loadS() {
         try {
@@ -44,7 +94,7 @@ public class loggBokModell {
             e.printStackTrace();
         }
     }
-
+/*
     public void save() {
         try {
             ObjectOutputStream outs = new ObjectOutputStream(new FileOutputStream(loggfile));
@@ -55,7 +105,7 @@ public class loggBokModell {
             e.printStackTrace();
         }
     }
-
+*/
     public ArrayList<loggEntry> getLoggentrys() {
         return loggentrys;
     }
